b4ff480b3f0a65a6c1423ad52d32d2759f907570
Sip <sipg789@gmail.com>
2022-06-06 17:05:21 -0500

    run webpack fix errors

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require("webpack");
+
+
+plugins: [
+    new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+],
+
+
+module.exports = {
+  entry: './assets/js/script.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  mode: 'development'
+};
